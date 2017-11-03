78c46636f4e91aeb71adb6a5f51e0446766dba03
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-11-03 12:08:27 +0100

    Adds intellij templates

--- setup.sh
+++ setup.sh
@@ -26,7 +26,8 @@ ln -s ~/DEV/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\
 ln -s ~/DEV/dotfiles/sublime/console.log.sublime-snippet ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/console.log.sublime-snippet
 ln -s ~/DEV/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/DEV/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/DEV/dotfiles/intellij/keymaps/mattiaspalmgren-keymap.xml ~/Library/Preferences/IdeaIC2017.2/keymaps
-ln -s ~/DEV/dotfiles/intellij/options/macros.xml ~/Library/Preferences/IdeaIC2017.2/options
+ln -s ~/DEV/dotfiles/intellij/keymaps ~/Library/Preferences/IdeaIC2017.2/keymaps
+ln -s ~/DEV/dotfiles/intellij/options ~/Library/Preferences/IdeaIC2017.2/options
+ln -s ~/DEV/dotfiles/intellij/templates ~/Library/Preferences/IdeaIC2017.2/templates
 ln -s ~/DEV/dotfiles/visual-studio-code/keybindings.json ~/Library/Application Support/Code/User/
 
