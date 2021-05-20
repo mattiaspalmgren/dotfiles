5410f9e5c61fdab32c334424d852c6c0367fefa4
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2021-05-20 18:47:01 +0200

    [shell] Update shell config
    The order of sourced files mattered in the case of using fzf for
    searching via ctrl+r
    Remove integration file, which I didn't find the reason for

--- shell/.zshrc
+++ shell/.zshrc
@@ -1,14 +1,17 @@
 export ZSH=/Users/mattiaspalmgren/.oh-my-zsh
 
-plugins=(git, z)
+plugins=(
+    git
+    z
+)
 source $ZSH/oh-my-zsh.sh
 
 export DOTFILES_SHELL_DIR=~/dev/dotfiles/shell
 
 SHELL_FILES=(
     envars.sh
-    vendor.zsh
     config.zsh
+    vendor.zsh
     aliases.sh
     functions.sh
 )
@@ -16,5 +19,3 @@ SHELL_FILES=(
 for FILE in $SHELL_FILES; do
     source $DOTFILES_SHELL_DIR/$FILE
 done
-
-source ~/.iterm2_shell_integration.zsh
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,11 +1,8 @@
 # Aliases
 
 alias vim='nvim'
-alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
-alias j12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
-alias j14="export JAVA_HOME=`/usr/libexec/java_home -v 14`; java -version"
-alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
 alias linked="find node_modules -type l -maxdepth 3 | grep -v .bin"
 alias copy="history | tail -1 | cut -c8-999 | pbcopy"
 alias ws="webstorm ."
 alias ch="charm ."
+alias ~="cd ~"
