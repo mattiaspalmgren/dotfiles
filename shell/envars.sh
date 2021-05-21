990f2a42dd874e0bd5cdfb0cb16785409ae835d6
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2021-05-21 16:50:14 +0200

    [shell] Update path

--- shell/envars.sh
+++ shell/envars.sh
@@ -2,10 +2,7 @@
 
 # Path
 export PATH=~/.nvm/versions/node/v14.17.0/bin:$PATH
-export PATH=~/Library/Python/3.7/bin:$PATH
-export PATH=~/Library/Python/3.8/bin:$PATH
 export PATH=~/dev/dotfiles/scripts:$PATH
-export PATH=~/dev/dotfiles/scripts/svt:$PATH
 
 # Settings
 export LC_ALL=en_US.UTF-8
