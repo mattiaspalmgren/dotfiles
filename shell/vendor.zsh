eb2c7dff328f564606b314aacd121356a2107099
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2021-05-24 17:38:17 +0200

    [shell] Update vendor

--- shell/vendor.zsh
+++ shell/vendor.zsh
@@ -1,4 +1,4 @@
-# Activation of vendor software
+# Vendor software related stuff
 
 # Fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -8,5 +8,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
 
 # Pyenv
+export PATH="$(pyenv root)/libexec/pyenv:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# Google cloud
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
+# Postgres
+export PATH=/Applications/Postgres.app/Contents/Versions/10/bin:$PATH
+export PATH="/usr/local/opt/node@14/bin:$PATH"
