4959da094358ab91617b3952438b6d3f8f2b2b20
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2021-08-09 14:18:16 +0200

    [shell] Add go to path

--- shell/envars.sh
+++ shell/envars.sh
@@ -3,6 +3,8 @@
 # Path
 export PATH=~/.nvm/versions/node/v14.17.0/bin:$PATH
 export PATH=~/dev/dotfiles/scripts:$PATH
+export PATH=/usr/local/opt/openjdk/bin:$PATH
+export PATH=~/go/bin:$PATH
 
 # Settings
 export LC_ALL=en_US.UTF-8
