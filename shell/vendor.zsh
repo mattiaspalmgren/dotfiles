6b7cbeff3437fbaa1033976ccb33849a469bfee2
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2022-08-19 09:34:25 +0200

    [shell] Update vendor paths

--- shell/vendor.zsh
+++ shell/vendor.zsh
@@ -15,6 +15,10 @@ eval "$(pyenv virtualenv-init -)"
 # Google cloud
 source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+export CLOUDSDK_PYTHON="/Users/mattiaspalmgren/.pyenv/versions/3.9.0/bin/python3"
 
 # Postgres
 export PATH=/Applications/Postgres.app/Contents/Versions/10/bin:$PATH
+
+# Ruby
+export PATH=/usr/local/lib/ruby/gems/3.1.0/bin:$PATH
