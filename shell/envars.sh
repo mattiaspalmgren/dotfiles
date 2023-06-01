5bc10330a0e4a6b3f8c840c672377e37da6c243e
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2023-06-01 15:49:36 +0200

    [shell] Update environment variables

--- shell/envars.sh
+++ shell/envars.sh
@@ -5,12 +5,14 @@ export PATH=~/.nvm/versions/node/v18.9.0/bin:$PATH
 export PATH=~/dev/dotfiles/scripts:$PATH
 export PATH=/usr/local/opt/openjdk/bin:$PATH
 export PATH=~/go/bin:$PATH
+export PATH=~/.local/bin:$PATH
 
 # Settings
 export LC_ALL=en_US.UTF-8
 export KEYTIMEOUT=1
 export VISUAL=nvim
 export EDITOR="$VISUAL"
+export LANG="en_US.UTF-8"
 
 # Custom
 export DOTFILES_DIR=~/dev/dotfiles/
