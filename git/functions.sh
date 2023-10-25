00882fee32d208dd46b6be764e44c3e4df7cbda1
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2023-10-25 11:00:35 +0200

    [git] Refactor size alias to also consider main branch

--- git/functions.sh
+++ git/functions.sh
@@ -11,3 +11,17 @@ function fixup() {
   git commit --fixup $HASH
   GIT_SEQUENCE_EDITOR=true git rebase --interactive --autosquash $HASH^
 }
+
+function size() {
+    if git rev-parse --verify master >/dev/null 2>&1; then
+        BRANCH=master
+    elif git rev-parse --verify main >/dev/null 2>&1; then
+        BRANCH=main
+    else
+        echo "Neither 'master' nor 'main' branch exists."
+        return 1
+    fi
+
+    SIZE=$(git diff $BRANCH..HEAD --stat | tail -1)
+    echo ${SIZE}
+}
