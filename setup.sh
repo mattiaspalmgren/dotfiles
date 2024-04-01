7caac35a4655d5956466ea8534e35005b55bf7f1
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2024-04-01 11:13:34 +0200

    [setup] Update setup script

--- setup.sh
+++ setup.sh
@@ -4,21 +4,25 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" ;
+sudo chown -R mattiaspalmgren /usr/local/share/zsh /usr/local/share/zsh/site-functions
+chmod u+w /usr/local/share/zsh /usr/local/share/zsh/site-functions
 
 # Install tools
-brew bundle Brewfile
+brew bundle install
+brew bundle --file=~/dev/dotfiles/Brewfile-alva
 
 # Apply modified osx defaults
 bash macos
 
-# Remove all applicaitons from dock
+# Remove all applications from dock
 dockutil --no-restart --remove all
 killall Dock
 
 # Create symlinks for dotfiles
 ln -s ~/dev/dotfiles/shell/.zshrc ~/.zshrc
 ln -s ~/dev/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/dev/dotfiles/git/.gitconfig-alva ~/.gitconfig-alva
 ln -s ~/dev/dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -s ~/dev/dotfiles/visual-studio-code/keybindings.json ~/Library/Application Support/Code/User/
 ln -s ~/dev/dotfiles/visual-studio-code/settings.json ~/Library/Application Support/Code/User/
